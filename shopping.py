32572355c91cc6fe238001ef3d33a52949b2275b
unknown <ryanadalal@gmail.com>
2021-08-21 15:43:50 -0400

    Working Load Data Function

--- shopping.py
+++ shopping.py
@@ -9,7 +9,7 @@ TEST_SIZE = 0.4
 
 def main():
 
-    # Check command-line arguments
+    # Check command-line argumentsa
     if len(sys.argv) != 2:
         sys.exit("Usage: python shopping.py data")
 
@@ -59,8 +59,36 @@ def load_data(filename):
     labels should be the corresponding list of labels, where each label
     is 1 if Revenue is true, and 0 otherwise.
     """
-    raise NotImplementedError
+    evidence = []
+    labels = []
+    with open(filename) as f:
+        reader = csv.DictReader(f)
+        counter = 0
+        for row in reader:
+            evidence.append([
+                int(row["Administrative"]), #int
+                float(row["Administrative_Duration"]), #float
+                int(row["Informational"]), #int
+                float(row["Informational_Duration"]), #float
+                int(row["ProductRelated"]), #int
+                float(row["ProductRelated_Duration"]), #float
+                float(row["BounceRates"]), #float
+                float(row["ExitRates"]), #float
+                float(row["PageValues"]), #float
+                float(row["SpecialDay"]), #float
+                int(0 if row["Month"] == "Jan" else 1 if row["Month"] == "Feb" else 2 if row["Month"] == "Mar" else 3 if row["Month"] == "Apr" else 4 if row["Month"] == "May" else 5 if row["Month"] == "June" else 6 if row["Month"] == "Jul" else 7 if row["Month"] == "Aug" else 8 if row["Month"] == "Sep" else 9 if row["Month"] == "Oct" else 10 if row["Month"] == "Nov" else 11 if row["Month"] == "Dec" else "failiure"), #int
+                int(row["OperatingSystems"]), #int
+                int(row["Browser"]), #int
+                int(row["Region"]), #int
+                int(row["TrafficType"]), #int
+                int(1 if row["VisitorType"] == "Returning_Visitor" else 0), #int
+                int(1 if row["Weekend"] == "TRUE" else 0) #int
+            ])
+            labels.append(row["Revenue"])
+            counter += 1
 
+    print(evidence[0])
+    return (evidence, labels)
 
 def train_model(evidence, labels):
     """
